d0690ac34f1f08a2069ca3c7857429b53820bb8f
Mahmudul Alam <mahmudula2000@gmail.com>
2023-11-10 11:57:00 +0600

    created project structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-gray-600 text-gray-100">
+      <nav className="flex justify-between items-center w-full px-10 py-4">
+        <div>My Site</div>
+        <div className="flex gap-10">
+          <Link href="/">Home</Link>
+          <Link href="/create-user">Create User</Link>
+          <Link href="/client-member">Client Member</Link>
+          <Link href="/member">Member</Link>
+          <Link href="/public">Public</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
